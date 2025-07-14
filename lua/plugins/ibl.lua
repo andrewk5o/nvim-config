fcc7a01b53b2ef4023d4cb3371a393b68bc9acca
Andrii Kliuiko <32270840+andrewk5o@users.noreply.github.com>
2025-07-14 14:45:37 +0200

    feat: add indent-blankline plugin configuration
    - Add ibl.lua plugin for indentation guides
    - Update lazy-lock.json with new dependency

---
+++ lua/plugins/ibl.lua
@@ -0,0 +1,17 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = { char = "│" },
+        whitespace = {
+            highlight = { "Whitespace", "NonText" },
+            remove_blankline_trail = false,
+        },
+        scope = {
+            enabled = true,
+            show_start = false,
+            show_end = false,
+        },
+    }
+}
+
